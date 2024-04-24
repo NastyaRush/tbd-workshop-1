6c62c317b3a63048cc1922982311bda0d70fa491
NastyaRush <86784750+NastyaRush@users.noreply.github.com>
2024-04-24 23:40:33 +0200

    Update dataproc main.tf

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -51,7 +51,7 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
     }
 
 preemptible_worker_config {
-      num_instances  = 1
+      num_instances  = 2
       preemptibility = "SPOT"
     }
 
