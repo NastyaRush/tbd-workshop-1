ee010722240ed6eb91024fb2156c4baed311ffc9
arasheus <a.rasheuskaya@stud.elka.pw.edu.pl>
2024-04-25 06:43:06 +0200

    add arbitrary machine type for vertex-ai and screenshots

--- modules/vertex-ai-workbench/variables.tf
+++ modules/vertex-ai-workbench/variables.tf
@@ -34,3 +34,8 @@ variable "ai_notebook_image_tag" {
   default = "latest"
 }
 
+variable "jupyterlab_instance_type" {
+  type        = string
+  default     = "e2-standard-2"
+  description = "Machine type for JupyterLab instance"
+}
