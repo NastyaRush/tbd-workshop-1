f0f5880e0ac86e9d7ceb565bd7792e3fe452cf83
arasheus <a.rasheuskaya@stud.elka.pw.edu.pl>
2024-04-25 06:13:52 +0200

    Revert "add arbitrary machine type for jupiter"
    This reverts commit b548281fc5e305baebf57ea221b056a5ed9e5e06.

--- modules/vertex-ai-workbench/variables.tf
+++ modules/vertex-ai-workbench/variables.tf
@@ -34,8 +34,3 @@ variable "ai_notebook_image_tag" {
   default = "latest"
 }
 
-variable "ai_notebook_machine_type" {
-    type        = string
-    default     = "e2-medium"
-    description = "Machine type to use for both worker and master nodes"
-}
