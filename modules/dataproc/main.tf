ad195704e6d5eafc5a65919032e0e91c468d2290
arasheus <a.rasheuskaya@stud.elka.pw.edu.pl>
2024-04-29 03:45:37 +0200

    modify dataproc main

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -53,6 +53,10 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
 preemptible_worker_config {
       num_instances  = 2
       preemptibility = "SPOT"
+      disk_config {
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = 30
+      }
     }
 
   }
